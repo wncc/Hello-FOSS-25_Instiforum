dc94d8f142d60fe23a74ce144a010b7eba602186
icodeforlife24 <shiviutkarsh@gmail.com>
2025-08-27 19:54:02 +0530

    Add Navbar component and update layout
    Introduced a new Navbar component and integrated it into the main layout. Removed font imports and related classes from layout.js, and cleaned up app/page.js by removing the default content.

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "../components/navbar";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='flex justify-between items-center p-4 bg-amber-100 text-white'>
+        <h1 className='text-3xl font-bold text-black'>Instiforum</h1>
+        <input type="text" placeholder='Search' className='px-4 py-2 rounded-full w-1/3 bg-white text-black'/>
+        <div >
+            <button className='bg-blue-500 flex w-40 justify-around items-center text-white px-4 py-2 rounded-full mr-2'> <img src='' className='rounded-full h-10 w-10'></img>Profile</button>
+        </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default Navbar
